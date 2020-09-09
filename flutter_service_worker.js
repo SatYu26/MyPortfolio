53f519ae97d1a2fa5a5a9c5297b0675763216297
SatYu26 <goyalsatyam8@gmail.com>
2020-09-10 03:32:31 +0530

    ...

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "cc9bf8317b74ab648c7fe0a355455f49",
 "/": "cc9bf8317b74ab648c7fe0a355455f49",
-"main.dart.js": "e482b98e6c7de9b19a2d8c45db1419b8",
+"main.dart.js": "b19253cd5721c9b430e1cc0077078593",
 "manifest.json": "9d00c562c906c169ad81b73ea6305524"
 };
 
